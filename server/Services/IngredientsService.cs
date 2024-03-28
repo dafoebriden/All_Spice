23220e271f49c6d32ce05dfb242a0ed2283d1857
Briden Dafoe <dafoebriden@gmail.com>
2024-03-27 20:15:31 -0600

    started services

--- server/Services/IngredientsService.cs
+++ server/Services/IngredientsService.cs
@@ -1,7 +1,30 @@
-namespace All_Spice.Services
-{
-    public class IngredientsService
-    {
+namespace All_Spice.Services;
 
+public class IngredientsService
+{
+
+    private readonly IngredientsRepository _repository;
+    public IngredientsService(IngredientsRepository repository)
+    {
+        _repository = repository;
+    }
+    internal Ingredient Create(Ingredient ingredientData)
+    {
+        Ingredient ingredient = _repository.Create(ingredientData);
+        return ingredient;
+    }
+    internal List<Ingredient> Get()
+    {
+        List<Ingredient> ingredients = _repository.Get();
+        return ingredients;
+    }
+    internal Ingredient Delete(int ingredientId, string userId)
+    {
+        Ingredient ingredientToDelete = _repository.GetIngredientById(IngredientId);
+        if (ingredientToDelete.CreatorId != userId)
+        {
+            throw new Exception("You do not have authorization to delete this ingredient");
+        }
+        _repository.Delete(ingredientId);
     }
 }
--- server/Services/RecipesService.cs
+++ server/Services/RecipesService.cs
@@ -1,7 +1,29 @@
-namespace All_Spice.Services
-{
-    public class RecipesService
-    {
+namespace All_Spice.Services;
 
+public class RecipesService
+{
+    private readonly RecipesRepository _repository;
+    public RecipesService(RecipesRepository repository)
+    {
+        _repository = repository;
+    }
+    internal Recipe Create(Recipe RecipeData)
+    {
+        Recipe Recipe = _repository.Create(RecipeData);
+        return Recipe;
+    }
+    internal List<Recipe> Get()
+    {
+        List<Recipe> Recipes = _repository.Get();
+        return Recipes;
+    }
+    internal Recipe Delete(int RecipeId, string userId)
+    {
+        Recipe RecipeToDelete = _repository.GetRecipeById(RecipeId);
+        if (RecipeToDelete.CreatorId != userId)
+        {
+            throw new Exception("You do not have authorization to delete this Recipe");
+        }
+        _repository.Delete(RecipeId);
     }
 }
